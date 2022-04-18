8d76fdb3a31de3bb7609ce98b40f736f171cc7a5
GarondEisenfaust <chr.schr@web.de>
2022-04-18 17:30:03 +0200

    adds a test

--- src/test/kotlin/com/scdregistry/searchprovider/SearchproviderApplicationTests.kt
+++ src/test/kotlin/com/scdregistry/searchprovider/SearchproviderApplicationTests.kt
@@ -1,13 +1,13 @@
 package com.scdregistry.searchprovider
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class SearchproviderApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun test() {
+        assertEquals(5, 2 + 3)
+    }
 }
