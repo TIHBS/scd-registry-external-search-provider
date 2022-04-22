ed2c4b3c8a6b26f5ec08a45d8f46bb189dd179fc
GarondEisenfaust <chr.schr@web.de>
2022-04-22 15:20:59 +0200

    implements storage in elastic search

--- src/RegistryEventHandler.ts
+++ src/RegistryEventHandler.ts
@@ -1,6 +1,7 @@
 import { BigNumber } from "ethers";
 import { Registry } from "../external/decentralised-scd-registry/src/types/Registry.js";
 import { IEventData, IRegistryEventHandler } from "./IRegistryEventHandler.js";
+import { Client as ElasticsearchClient } from "@elastic/elasticsearch";
 import fetch from "node-fetch";
 
 interface EventData {
@@ -9,12 +10,36 @@ interface EventData {
 
 export class RegistryEventHandler implements IRegistryEventHandler {
   private registry: Registry;
+  private elasticsearchClient: ElasticsearchClient;
+  private elasticsearchIndex: string;
 
-  constructor(registry: Registry) {
+  constructor(
+    registry: Registry,
+    elasticsearchClient: ElasticsearchClient,
+    elasticsearchIndex = "scds"
+  ) {
     this.registry = registry;
+    this.elasticsearchClient = elasticsearchClient;
+    this.elasticsearchIndex = elasticsearchIndex;
   }
 
   async onEvent(eventData: IEventData): Promise<boolean> {
-    throw new Error("Not implemented!");
+    const id = (eventData as EventData).id;
+    const scdMetadata = await this.registry.retrieveById(id);
+    const onlyMetadata = scdMetadata.metadata;
+    const url = process.env.PROXY_URL + onlyMetadata.url;
+    console.log(`Fetching SCD from ${url}`);
+    const scd = await (await fetch(url)).json();
+    console.log(
+      `Fetched SCD of a smart contract with the name: ${scd["Name"]} and the Hash ${scd["Hash"]}`
+    );
+
+    this.elasticsearchClient.index({
+      index: this.elasticsearchIndex,
+      document: scd,
+    });
+    console.log(`Stored ${scd["Name"]}`);
+
+    return true;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,42 +2,55 @@ import { ethers } from "ethers";
 import { RegistryEventListener } from "./RegistryEventListener.js";
 import { ExpressServer } from "./ExpressServer.js";
 import { RegistryEventHandler } from "./RegistryEventHandler.js";
-
+import { Provider } from "@ethersproject/abstract-provider/lib/index";
 import RegistryDeployment from "../external/decentralised-scd-registry/deployments/ganache-cli/Registry.json";
 import { Registry__factory } from "../external/decentralised-scd-registry/src/types/factories/Registry__factory.js";
-import "dotenv/config";
 import { Client as ElasticsearchClient } from "@elastic/elasticsearch";
+import "dotenv/config";
 
-async function main() {
-  const elasticsearchUrl = process.env.ELASTICSEARCH_URL
+function createElasticsearchClient(
+  elasticsearchUrl = process.env.ELASTICSEARCH_URL
     ? process.env.ELASTICSEARCH_URL
-    : "http://localhost:9200";
-
+    : "http://localhost:9200"
+): ElasticsearchClient {
   console.log(`Connecting to Elasticsearch at ${elasticsearchUrl}`);
-  const elasticsearchClient = new ElasticsearchClient({
+  return new ElasticsearchClient({
     node: elasticsearchUrl,
   });
+}
 
-  const ethereumNetworkUrl = process.env.ETHEREUM_NETWORK_URL
+async function createProvider(
+  ethereumNetworkUrl = process.env.ETHEREUM_NETWORK_URL
     ? process.env.ETHEREUM_NETWORK_URL
-    : "http://localhost:8545";
-
+    : "http://localhost:8545"
+) {
   console.log(`Connecting to Ethereum network at ${ethereumNetworkUrl}`);
+  return await ethers.getDefaultProvider(ethereumNetworkUrl);
+}
 
-  const contractAddress = process.env.REGISTRY_ADDRESS
+function createRegistryContract(
+  provider: Provider,
+  contractAddress = process.env.REGISTRY_ADDRESS
     ? process.env.REGISTRY_ADDRESS
-    : RegistryDeployment.address;
-
+    : RegistryDeployment.address
+) {
   console.log(`Connecting to Registry contract at ${contractAddress}`);
-  const signer = await ethers.getDefaultProvider(ethereumNetworkUrl);
-  const registry = Registry__factory.connect(contractAddress, signer);
+  return Registry__factory.connect(contractAddress, provider);
+}
+
+async function main() {
+  const provider = await createProvider();
+  const registry = createRegistryContract(provider);
 
   const registryEventListener = new RegistryEventListener(
     registry,
     registry.filters.ContractRegistered()
   );
 
-  registryEventListener.subscribe(new RegistryEventHandler(registry));
+  const elasticsearchClient = createElasticsearchClient();
+  registryEventListener.subscribe(
+    new RegistryEventHandler(registry, elasticsearchClient)
+  );
   registryEventListener.start();
 
   const expressServer = new ExpressServer();
