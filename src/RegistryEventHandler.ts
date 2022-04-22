5211c2e7ecdd04abef27f7426f79023f517ab8bd
GarondEisenfaust <chr.schr@web.de>
2022-04-22 17:16:13 +0200

    implements fetching and storage

--- src/RegistryEventHandler.ts
+++ src/RegistryEventHandler.ts
@@ -2,6 +2,7 @@ import { BigNumber } from "ethers";
 import { Registry } from "../external/decentralised-scd-registry/src/types/Registry.js";
 import { IEventData, IRegistryEventHandler } from "./IRegistryEventHandler.js";
 import { Client as ElasticsearchClient } from "@elastic/elasticsearch";
+import { SCD } from "../external/decentralised-scd-registry-common/SCD";
 import fetch from "node-fetch";
 
 interface EventData {
@@ -25,21 +26,31 @@ export class RegistryEventHandler implements IRegistryEventHandler {
 
   async onEvent(eventData: IEventData): Promise<boolean> {
     const id = (eventData as EventData).id;
+    const scd = await this.fetchSCD(id);
+
+    this.elasticsearchClient.index({
+      index: this.elasticsearchIndex,
+      document: scd,
+    });
+    console.log(`Stored ${scd.name}`);
+
+    return true;
+  }
+
+  private async fetchSCD(id: BigNumber): Promise<SCD> {
     const scdMetadata = await this.registry.retrieveById(id);
     const onlyMetadata = scdMetadata.metadata;
+
+    if (!onlyMetadata.isValid) {
+      throw new Error("No SCD with this id exists!");
+    }
+
     const url = process.env.PROXY_URL + onlyMetadata.url;
     console.log(`Fetching SCD from ${url}`);
     const scd = await (await fetch(url)).json();
     console.log(
       `Fetched SCD of a smart contract with the name: ${scd["Name"]} and the Hash ${scd["Hash"]}`
     );
-
-    this.elasticsearchClient.index({
-      index: this.elasticsearchIndex,
-      document: scd,
-    });
-    console.log(`Stored ${scd["Name"]}`);
-
-    return true;
+    return scd;
   }
 }
