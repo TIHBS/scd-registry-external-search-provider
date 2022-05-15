8e9f27397ece67eaae24b24a387c2598a3f5ed65
GarondEisenfaust <chr.schr@web.de>
2022-05-15 11:26:09 +0200

    adds tests for onEvent

--- test/RegistryEventHandler.test.ts
+++ test/RegistryEventHandler.test.ts
@@ -1,6 +1,5 @@
 import expect from "./expect";
 import { Client as ElasticsearchClient } from "@elastic/elasticsearch";
-import { ElasticsearchEnvironment } from "./ElasticsearchEnvironment";
 import { RegistryEventHandler } from "../src/RegistryEventHandler";
 import { Registry } from "../external/decentralised-scd-registry-common/src/wrappers/Registry";
 import { SwarmClient } from "../src/SwarmClient";
@@ -9,63 +8,85 @@ import { BigNumber } from "ethers";
 import { cloneDeep } from "lodash";
 
 describe("RegistryEventHandler", () => {
-  let elasticsearchClient: ElasticsearchClient;
-  let elasticsearchEnvironment: ElasticsearchEnvironment;
   let swarmClientMock: SwarmClient;
   let registryMock: Registry;
+  let elasticsearchClientMock: ElasticsearchClient;
+  let eventHandler: RegistryEventHandler;
+
+  const theId = BigNumber.from(1);
+  const theMetadata = {
+    id: theId,
+    metadata: {
+      name: "Railway",
+      author: "The Tank Engine",
+      internalAddress: "0x2222",
+      url: "TO BE CHANGED",
+      signature: "dasffafjafajfkla",
+      version: "2.2",
+      functions: [],
+      events: [],
+      isValid: true,
+      blockChainType: 0,
+    },
+  };
+
+  const expected = {
+    name: "Railway",
+    author: "The Tank Engine",
+    hash: "jhadsöklfjaklmkknaöoie4375843z90jesafea#+afdaf",
+  };
 
   before(async function () {
-    this.timeout(300000);
-
-    elasticsearchEnvironment = new ElasticsearchEnvironment();
-
-    await elasticsearchEnvironment.start();
-    const containerUrl = elasticsearchEnvironment.getUrl();
-    elasticsearchClient = new ElasticsearchClient({ node: containerUrl });
-
     swarmClientMock = { fetch: () => {} } as any as SwarmClient;
     registryMock = {
       retrieveById: () => {},
     } as any as Registry;
+    elasticsearchClientMock = {
+      index: (input: any | any[]) => {},
+    } as any as ElasticsearchClient;
   });
 
-  after(async function () {
-    this.timeout(300000);
-    await elasticsearchEnvironment.down();
+  beforeEach(() => {
+    eventHandler = new RegistryEventHandler(
+      registryMock,
+      elasticsearchClientMock,
+      swarmClientMock
+    );
+  });
+
+  describe("onEvent", () => {
+    it("should return true", async () => {
+      const metadata = cloneDeep(theMetadata);
+      metadata.metadata.url = "http://localhost:4242";
+      sinon.stub(registryMock, "retrieveById").returns(metadata);
+      sinon.stub(eventHandler, "fetchFromWeb").returns(expected);
+
+      const result = await eventHandler.onEvent({ id: theId });
+      expect(result).to.be.true;
+    });
+
+    it("should also return true", async () => {
+      const metadata = cloneDeep(theMetadata);
+      metadata.metadata.url = "swarm://efsfeskfjesfnsekjfbeslkj";
+      sinon.stub(registryMock, "retrieveById").returns(metadata);
+      sinon.stub(swarmClientMock, "fetch").returns(expected);
+
+      const result = await eventHandler.onEvent({ id: theId });
+      expect(result).to.be.true;
+    });
+
+    it("should return false", async () => {
+      const metadata = cloneDeep(theMetadata);
+      metadata.metadata.url = "http://localhost:4242";
+      sinon.stub(registryMock, "retrieveById").returns(metadata);
+      sinon.stub(eventHandler, "fetchFromWeb").returns(expected);
+      sinon.stub(elasticsearchClientMock, "index").throws();
+      const result = await eventHandler.onEvent({ id: theId });
+      expect(result).to.be.false;
+    });
   });
 
   describe("fetchSCD", () => {
-    const theId = BigNumber.from(1);
-    const theMetadata = {
-      id: theId,
-      metadata: {
-        name: "Railway",
-        author: "The Tank Engine",
-        internalAddress: "0x2222",
-        url: "TO BE CHANGED",
-        signature: "dasffafjafajfkla",
-        version: "2.2",
-        functions: [],
-        events: [],
-        isValid: true,
-        blockChainType: 0,
-      },
-    };
-    const expected = {
-      name: "Railway",
-      author: "The Tank Engine",
-      hash: "jhadsöklfjaklmkknaöoie4375843z90jesafea#+afdaf",
-    };
-    let eventHandler: RegistryEventHandler;
-
-    beforeEach(() => {
-      eventHandler = new RegistryEventHandler(
-        registryMock,
-        elasticsearchClient,
-        swarmClientMock
-      );
-    });
-
     it("should fetch the scd from swarm", async () => {
       const metadata = cloneDeep(theMetadata);
       metadata.metadata.url = "swarm://efsfeskfjesfnsekjfbeslkj";
@@ -82,6 +103,15 @@ describe("RegistryEventHandler", () => {
       sinon.stub(eventHandler, "fetchFromWeb").returns(expected);
       const result = await eventHandler.fetchSCD(theId);
       expect(result).to.deep.equal(expected);
+    });
+
+    it("should should be rejected because the SCD is invalid", async () => {
+      const metadata = cloneDeep(theMetadata);
+      metadata.metadata.isValid = false;
+      sinon.stub(registryMock, "retrieveById").returns(metadata);
+      await expect(eventHandler.fetchSCD(theId)).to.be.rejectedWith(
+        "No SCD with this id exists!"
+      );
     });
   });
 });
--- test/expect.ts
+++ test/expect.ts
@@ -1,5 +1,7 @@
 import chai from "chai";
+import ChaiAsPromised from "chai-as-promised";
 
 const { expect } = chai;
+chai.use(require("chai-as-promised"));
 
 export default expect;
